5c31589b904b2fbf8845d2ae144c5f6443910be1
Igor Stein <70313147+becks-boo@users.noreply.github.com>
2022-11-09 18:12:14 +0100

    added drag and drop to index page

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const path = require("path");
 const PORT = process.env.PORT || 6000;
-
 const app = express();
+const indexRouter = require("./routes/index");
+
+app.engine("handlebars", handlebars.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
 
 app.listen(PORT, () => {
     console.log(`File-Sorting app listening on port ${PORT}`);
 })
+
+app.use("/", indexRouter);
---
+++ src/main.js
@@ -0,0 +1,56 @@
+document.querySelectorAll(".drop-zone-input").forEach(inputElement => {
+    const dropZoneElement = inputElement.closest(".drop-zone");
+
+    dropZoneElement.addEventListener("click", e => {
+        inputElement.click();
+    });
+
+    inputElement.addEventListener("change", e => {
+        if (inputElement.files.length) {
+            updateThumbnail(dropZoneElement, inputElement.files[0]);
+        }
+    });
+
+    dropZoneElement.addEventListener("dragover", e => {
+        e.preventDefault();
+        dropZoneElement.classList.add("drop-zone-over");
+    });
+
+    ["dragleave", "dragend"].forEach(type => {
+        dropZoneElement.addEventListener(type, e => {
+            dropZoneElement.classList.remove("drop-zone-over");
+        })
+    });
+
+    dropZoneElement.addEventListener("drop", e => {
+        e.preventDefault();
+
+        if (e.dataTransfer.files.length) {
+            inputElement.files = e.dataTransfer.files;
+            updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+        }
+
+        dropZoneElement.classList.remove("drop-zone-over");
+    });
+});
+
+/**
+ * @param { HTMLElement } dropZoneElement
+ * @param { File } file
+ */
+
+function updateThumbnail(dropZoneElement, file) {
+    let thumbnailElement = dropZoneElement.querySelector(".drop-zone-thumb");
+
+    if (dropZoneElement.querySelector(".drop-zone-prompt")) {
+        dropZoneElement.querySelector(".drop-zone-prompt").remove();
+    }
+
+    if (!thumbnailElement) {
+        thumbnailElement = document.createElement("div");
+        thumbnailElement.classList.add("drop-zone-thumb");
+        dropZoneElement.appendChild(thumbnailElement);
+    }
+
+    thumbnailElement.dataset.label = file.name;
+}
